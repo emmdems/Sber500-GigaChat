5643f9dd2b4884eee3f0a7db4b9d58d34485dc31
Dmitry Biryukov <dmitrybiryukov4@gmail.com>
2024-08-04 00:27:45 +0300

    draft 2.0

--- src/main.py
+++ src/main.py
@@ -1,9 +1,24 @@
 import os
 from langchain.chat_models.gigachat import GigaChat
+from langchain.schema import HumanMessage, SystemMessage
 
 # Получение переменной окружения
-giga_chat_client_id = os.getenv("GigaChat_Client_ID")
+#F5 для запуска, чтобы переменная из json подтянулась
+giga_chat_auth = os.getenv("GigaChat_Auth")
+#data=[Model(id_='GigaChat', object_='model', owned_by='salutedevices'), Model(id_='GigaChat-Plus', object_='model', owned_by='salutedevices'), Model(id_='GigaChat-Pro', object_='model', owned_by='salutedevices')] object_='list'
+chat = GigaChat(model="GigaChat", credentials=giga_chat_auth);
 
-chat = GigaChat(model="GigaChat-Lite", credentials=giga_chat_client_id, verify_ssl_certs=False);
+#print(chat.get_num_tokens("Сколько токенов в этой строке"));
 
-print(chat.get_num_tokens("Сколько токенов в этой строке"));
+messages = [
+    SystemMessage(
+        content="Ты эмпатичный бот-психолог, который помогает пользователю решить его проблемы."
+    )
+]
+
+while(True):
+    user_input = input("User: ")
+    messages.append(HumanMessage(content=user_input))
+    res = chat(messages)
+    messages.append(res)
+    print("Bot: ", res.content)
